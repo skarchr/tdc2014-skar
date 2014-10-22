be13a3ced60adfe0acb0fede7dd69f254990592b
Christian Skar <skarchr@gmail.com>
2014-10-22 23:11:12 +0200

    Fixed title text

--- public/app/js/app.js
+++ public/app/js/app.js
@@ -2,9 +2,18 @@
     'use strict';
 
     var app = angular.module('app', ['ngRoute', 'ngAnimate'])
-        .run(function($rootScope){
+        .run(['$rootScope',function($rootScope){
             $rootScope.namePattern = /^[a-zA-ZàáâäãåąčćęèéêëėįìíîïłńòóôöõøùúûüųūÿýżźñçčšžÀÁÂÄÃÅĄĆČĖĘÈÉÊËÌÍÎÏĮŁŃÒÓÔÖÕØÙÚÛÜŲŪŸÝŻŹÑßÇŒÆČŠŽ∂ð ]+$/;
-    });
+
+            $rootScope.$on("$routeChangeSuccess", function (event, currentRoute, previousRoute) {
+
+                document.title = 'ngMan';
+
+                if(currentRoute.title !== undefined){
+                    document.title += ' - ' + currentRoute.title;
+                }
+            });
+    }]);
 
 
     app.config(['$routeProvider', '$locationProvider',
@@ -19,34 +28,42 @@
                     templateUrl: '/partials/views/menu'
                 }).
                 when('/init', {
+                    title:'Init',
                     templateUrl: '/partials/views/init',
                     controller: 'commonController'
                 }).
                 when('/binding', {
+                    title:'Binding',
                     templateUrl: '/partials/views/binding',
                     controller: 'bindingController'
                 }).
                 when('/directive', {
+                    title:'Directive',
                     templateUrl: '/partials/views/directive',
                     controller: 'directiveController'
                 }).
                 when('/form', {
+                    title:'Form',
                     templateUrl: '/partials/views/form',
                     controller: 'formController'
                 }).
                 when('/watch', {
+                    title:'Watch',
                     templateUrl: '/partials/views/watch',
                     controller: 'watchController'
                 }).
                 when('/filter', {
+                    title:'Filter',
                     templateUrl: 'partials/views/filter',
                     controller: 'filterController'
                 }).
                 when('/factory', {
+                    title:'Factory',
                     templateUrl: 'partials/views/factory',
                     controller: 'factoryController'
                 }).
                 when('/animate', {
+                    title:'Animate',
                     templateUrl: 'partials/views/animate',
                     controller: 'commonController'
                 }).
