8de939d84e9a5e89fe30335b361b44eb547609f2
Christian Skar <skarchr@gmail.com>
2014-10-22 21:08:12 +0200

    removed titles in partials

--- public/app/js/NavigationController.js
+++ public/app/js/NavigationController.js
@@ -5,9 +5,40 @@
 
         $scope.activePage = '';
 
+        $scope.activeImage = '';
+
 
         $scope.$watch(function() { return $location.path(); }, function(){
             $scope.activePage = $location.path().split('/')[1];
+
+            switch ($scope.activePage){
+                case 'init':
+                    $scope.activeImage = '/app/images/spark.png'
+                    break;
+                case 'binding':
+                    $scope.activeImage = '/app/images/snake.png'
+                    break;
+                case 'directive':
+                    $scope.activeImage = '/app/images/needle.png'
+                    break;
+                case 'form':
+                    $scope.activeImage = '/app/images/top.png'
+                    break;
+                case 'watch':
+                    $scope.activeImage = '/app/images/shadow.png'
+                    break;
+                case 'filter':
+                    $scope.activeImage = '/app/images/magnet.png'
+                    break;
+                case 'factory':
+                    $scope.activeImage = '/app/images/gemini.png'
+                    break;
+                case 'animate':
+                    $scope.activeImage = '/app/images/hard.png'
+                    break;
+                default:
+                    $scope.activeImage = '';
+            }
         });
 
     }]);
